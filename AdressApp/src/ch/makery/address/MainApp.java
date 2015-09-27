1a25dd2a7e398e4e15320f5cdd32897c45630322
dzianis <rym-dzianis@mail.ru>
2015-09-27 15:25:52 -0300

    123

--- AdressApp/src/ch/makery/address/MainApp.java
+++ AdressApp/src/ch/makery/address/MainApp.java
@@ -98,7 +98,7 @@ public class MainApp extends Application {
     }
 
     public static void main(String[] args) {
-    	System.out.println("Started !!! + 111");
+    	System.out.println("Started !!!");
         launch(args);
     }
 }
